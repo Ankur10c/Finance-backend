c213da3bf86e5827049c09a53e207443c8541679
Ankur10c <masterankur8888@gmail.com>
2026-04-06 18:13:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("DB Connected");
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
-    });
+    const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch(err => console.log(err));
   console.log("ENV CHECK:", process.env.MONGO_URI);
